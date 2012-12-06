b5f9b5873c0e18bbf0c0a118ab581ccfe06f0b76
Scott Feeney <scott@oceanbase.org>
2012-12-05 21:03:06 -0600

    Add a landing page

--- tenttest.py
+++ tenttest.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'index page!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
